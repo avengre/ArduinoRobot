d2da71e864f8506e4cecc39a46e7153909937f51
Jason <avengre@users.noreply.github.com>
2015-06-02 12:00:58 -0400

    - Additional Motor functions added

--- Motor.h
+++ Motor.h
@@ -24,37 +24,55 @@ public:
 		DirectionPin = inDirectionPin;
 		PowerPin = inPowerPin;
 		SensingPin = inSensingPin;
+		_setPinInputOutput();
+		setMotorForward(0);
 	};
-
 	bool getBrake(){ return bIsBraked; };
 	void setBrake(bool inBraked)
 	{
 		bIsBraked = inBraked;
 		digitalWrite(BrakePin, inBraked);
 	};
-
+	void BrakeOn(){ setBrake(HIGH); };
+	void BrakeOFF(){ setBrake(LOW); };
 	void setMotorForward(uchar inPower = 255)
 	{
+		if (!getIsInverted())  // Motor might be facing opposite direction!
+		{
+			digitalWrite(DirectionPin, HIGH);
+		}
+		else
+		{
+			digitalWrite(DirectionPin, LOW);
+		}
 
-		DirectionPin = HIGH;
 		setPower(inPower);
-		eMotorDirection = MOTOR_FORWARD;
+		eMotorDirection = MOTOR_FORWARD;  // For reference tho, this will be considered "forward" regardless as to what pin is set to
+
+
 	};
 	void setMotorBackward(uchar inPower = 255)
 	{
-		DirectionPin = LOW;
+		if (!getIsInverted())  // Motor might be facing opposite direction!
+		{
+			digitalWrite(DirectionPin, LOW);
+		}
+		else
+		{
+			digitalWrite(DirectionPin, HIGH);
+		}
 		setPower(inPower);
 		eMotorDirection = MOTOR_BACKWARD;
 	};
 	void setPower(uchar inPower)
 	{
-		PowerPin = inPower;
+		curPower = inPower;
+		analogWrite(PowerPin,inPower);
 	};
 	uchar getPower()
 	{
-		return PowerPin;
+		return curPower;
 	};
-
 	bool getIsInverted()
 	{
 		return bIsInverted;
@@ -64,8 +82,16 @@ public:
 		bIsInverted = inInverted;
 	};
 private:
+	void _setPinInputOutput()
+	{
+		pinMode(BrakePin, OUTPUT);
+		pinMode(DirectionPin, OUTPUT);
+		pinMode(PowerPin, OUTPUT);
+		pinMode(SensingPin, INPUT);
+	}
 	bool bIsInverted; // If motor will be facing opposite direction, will this be inverted  (IE, left motor and right motor, one will have to be inverted)
 	bool bIsBraked;
+	uchar curPower;
 	PinNum BrakePin, DirectionPin, PowerPin; // Digital
 	PinNum SensingPin;  // Analog
 	Direction eMotorDirection;
@@ -75,34 +101,79 @@ private:
 class MovementManager
 {
 public:
-	void SetMotors(MotorController Controller1, MotorController Controller2)
+	void SetMotors(MotorController inLeftMotor, MotorController inRightMotor)
 	{
-		LeftMotor = Controller1;
-		RightMotor = Controller2;
+		LeftMotor = inLeftMotor;
+		RightMotor = inRightMotor;
 	};
 	
-	void MoveForward(uchar inSpeed)
+	void MoveForward(uchar inSpeed=255)
 	{
+		LeftMotor.BrakeOFF();
+		RightMotor.BrakeOFF();
+		LeftMotor.setMotorForward(inSpeed);
+		RightMotor.setMotorForward(inSpeed);
 
-	}
-	void MoveBackward(uchar inSpeed)
+	};
+	void MoveBackward(uchar inSpeed=255)
 	{
-
-	}
+		LeftMotor.BrakeOFF();
+		RightMotor.BrakeOFF();
+		LeftMotor.setMotorBackward(inSpeed);
+		RightMotor.setMotorBackward(inSpeed);
+	};
 	void FullStop(bool UseBrake = false)
 	{
+		LeftMotor.setPower(0);
+		RightMotor.setPower(0);
+	};
 
-	}
+	// Turn off power to the motors, as there's no reason to apply power and brake
 	void BrakesOn()
 	{
+		LeftMotor.setPower(0);
+		RightMotor.setPower(0);
+		LeftMotor.BrakeOn();
+		RightMotor.BrakeOn();
+	};
 
-	}
 	void BrakesOff()
 	{
+		LeftMotor.BrakeOFF();
+		RightMotor.BrakeOFF();
+	};
 
-	}
+	// 0.0f = only right engine making the turn, left engine off
+	// 0.5f means right engine full power, left engine half
+	// 1.0f means yer gonna be going straight
+	void LeftTurn(uchar inPower,float OpposingEnginePower = 0.5f)
+	{
+		RightMotor.setMotorForward(inPower);
+		LeftMotor.setMotorForward(inPower * OpposingEnginePower);
+	};
+	// 1.0f = only left engine making the turn, 0.5f means left engine full power, right engine half
+	void RightTurn(uchar inPower, float OpposingEnginePower = 0.5f)
+	{
+		RightMotor.setMotorForward(inPower * OpposingEnginePower);
+		LeftMotor.setMotorForward(inPower);
+	};
 
+	void StaticTurnLeft(uchar inPower = 255)
+	{
+		LeftMotor.setMotorBackward(inPower);
+		RightMotor.setMotorForward(inPower);
+	};
+	void StaticTurnRight(uchar inPower = 255)
+	{
+		LeftMotor.setMotorForward(inPower);
+		RightMotor.setMotorBackward(inPower);
+	};
 
+	void SetSpeed(uchar inPower)
+	{
+		LeftMotor.setPower(inPower);
+		RightMotor.setPower(inPower);
+	};
 private:
 	MotorController LeftMotor, RightMotor;
 
--- RobotMotor.ino
+++ RobotMotor.ino
@@ -1,20 +1,34 @@
 #include "Motor.h"
 
+#define SPEED 175
+
 void setup()  // We'll control our own loops so we have deleted loop()
 {
 	MotorController LeftMotor,RightMotor;
-		LeftMotor.setPins(9, 12, 3, 0);
-		RightMotor.setPins(8, 13, 11, 1);
+	RightMotor.setPins(9, 12, 3, 0); // Channel A
+	LeftMotor.setPins(8, 13, 11, 1); // Channel B
+	
+	RightMotor.setIsInverted(false);
+	LeftMotor.setIsInverted(false);
 	
 	MovementManager mMovementManager;
-
 	mMovementManager.SetMotors(LeftMotor, RightMotor);
+	
 
 
+	
+	mMovementManager.BrakesOff();
+	mMovementManager.MoveForward(SPEED);
+	delay(5000);
+	mMovementManager.FullStop(false);
+	delay(2000);
+	mMovementManager.MoveBackward(SPEED * 0.75);
+	delay(7000);
+	mMovementManager.FullStop(false);
 
 
-
-
+	while (true){};
+	
 }
 
 void loop()
